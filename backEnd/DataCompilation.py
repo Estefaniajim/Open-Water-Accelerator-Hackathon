cc43f4a30846a11bad4a217d50ac045d01d33126
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-08-31 18:39:05 -0500

    Update DataCompilation.py

--- backEnd/DataCompilation.py
+++ backEnd/DataCompilation.py
@@ -1,1 +1,20 @@
-from flask import Flask, request, jsonify, redirect
+from flask import Flask, request, url_for, redirect
+
+app = Flask(__name__)
+userName = None
+userAddress = None
+userID = None
+userOrder = None
+
+def user():
+    global userName, userAddress, userID, userOrder
+
+@app.route('/login-form', methods=['GET', 'POST'])
+def getUserData():
+
+
+@app.route('/request-form', methods=['GET', 'POST'])
+def getInfoOrder():
+
+
+
