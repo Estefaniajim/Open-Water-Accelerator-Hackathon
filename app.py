f43dc4af7bc7afefe61990bed71c4a6bf5ae25e3
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-09-02 14:38:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from flas
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
+    if request.method == "POST":
+        #Fetch form data
+        userDetails = request.form
+        name = userDetails["name"]
+        email = userDetails["email"]
     return render_template("login.html")
 if __name__ == "__main__":
     app.run(debug=True)
