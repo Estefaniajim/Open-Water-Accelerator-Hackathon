41d3175da5b56c26be5975590b059976a8e39ac9
Brennan Wu <brennanwu@Brennans-MacBook-Pro.local>
2020-09-02 15:59:16 -0500

    Added Register function

--- backEnd/DataCompilation.py
+++ backEnd/DataCompilation.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, url_for, redirect, render_template
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
 # The user information
@@ -12,10 +14,44 @@ userActiveOrder = None
 orderRestaurant = None
 orderTime = None
 
+# Configure db
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
+
 def user():
     global userName, userAddress, userID, dataBasePassword, dataBaseUser
 def order():
     global userActiveOrderOrder, orderRestaurant, orderTime
+
+#Registering a new user
+@app.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirmPass = request.form["confirmPass"]
+        error = None
+        cur = mysql.connection.cursor()
+    if not username:
+        error = "Username is required."
+    elif not password:
+        error = "Password is required."
+    elif password != confirmPass:
+        error = "Passwords do not match."
+
+    if error is None:
+        cur.execute("INSERT INTO users(username, email, password) VALUES(%s, %s, %s)",(name, email, password))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for("auth.login"))
+
+    return render_template("register", error = error)
 
 # Getting the user data form the FE in login
 @app.route('/login-form', methods=['GET', 'POST'])
